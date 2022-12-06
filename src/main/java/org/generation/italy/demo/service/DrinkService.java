f9967fdda3ac04130540557474bcd907fadaa9da
Luca Spennato <luca.spennato@gmail.com>
2022-12-06 12:50:56 +0100

    service methods add

--- src/main/java/org/generation/italy/demo/service/DrinkService.java
+++ src/main/java/org/generation/italy/demo/service/DrinkService.java
@@ -1,5 +1,9 @@
 package org.generation.italy.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.generation.italy.demo.pojo.Drink;
 import org.generation.italy.demo.repo.DrinkRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +14,20 @@ public class DrinkService {
 	@Autowired
 	private DrinkRepo p;
 	
+	
+	public void save(Drink d) {
+		p.save(d);
+	}
+	
+	public List<Drink> findAll(){
+		return p.findAll();
+	}
+	
+	public Optional<Drink> findDrinkById(int id){
+		return p.findById(id);
+	}
+	
+	public void deleteDrink(Drink d) {
+		p.delete(d);
+	}
 }
